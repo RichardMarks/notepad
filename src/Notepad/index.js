d5704e9c1b0ba39629cafffd9dc1cac1fa6a3811
Richard Marks <ccpsceo@gmail.com>
2017-03-05 16:25:15 -0600

    insert character and backspace functionality

--- src/Notepad/index.js
+++ src/Notepad/index.js
@@ -6,32 +6,65 @@ import StatusBar from './StatusBar'
 
 import './index.css'
 
+const mockData = [
+  'Hello, World!',
+  'This is line two',
+  'This is a third line of more text',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  ''
+]
+
+const CURSOR_HOME = { row: 0, column: 0 }
+
+const KEY = {
+  END: 35,
+  HOME: 36,
+  LEFT: 37,
+  RIGHT: 39,
+  UP: 38,
+  DOWN: 40,
+  BACKSPACE: 8,
+  DELETE: 46,
+  ENTER: 13
+}
+
+const CURSOR_KEYS = [
+  KEY.UP,
+  KEY.DOWN,
+  KEY.LEFT,
+  KEY.RIGHT
+]
+
 export default class Notepad extends Component {
   constructor (props) {
     super(props)
 
     this.onKeyDown = this.onKeyDown.bind(this)
+    this.onKeyPress = this.onKeyPress.bind(this)
+
+    this.moveToStartOfLine = this.moveToStartOfLine.bind(this)
+    this.moveToEndOfLine = this.moveToEndOfLine.bind(this)
+    this.moveToTopOfDocument = this.moveToTopOfDocument.bind(this)
+    this.moveToBottomOfDocument = this.moveToBottomOfDocument.bind(this)
+    this.moveUp = this.moveUp.bind(this)
+    this.moveDown = this.moveDown.bind(this)
+    this.moveLeft = this.moveLeft.bind(this)
+    this.moveRight = this.moveRight.bind(this)
+    this.insertCarriageReturn = this.insertCarriageReturn.bind(this)
+    this.insertBackspace = this.insertBackspace.bind(this)
+    this.insertCharacter = this.insertCharacter.bind(this)
 
     this.state = {
-      documentCursor: {
-        row: 2,
-        column: 5
-      },
-      documentContent: [
-        'Hello, World!',
-        'This is line two',
-        'This is a third line of more text',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        ''
-      ]
+      documentCursor: CURSOR_HOME,
+      documentContent: mockData
     }
   }
 
@@ -39,175 +72,240 @@ export default class Notepad extends Component {
     this.topLevel.focus()
   }
 
-  onKeyDown (event) {
+  moveToStartOfLine (documentCursor, documentContent) {
+    documentCursor.column = 0
+  }
+
+  moveToEndOfLine (documentCursor, documentContent) {
+    if (documentContent[documentCursor.row].length) {
+      documentCursor.column = documentContent[documentCursor.row].length
+    } else {
+      documentCursor.column = 0
+    }
+  }
+
+  moveToTopOfDocument (documentCursor, documentContent) {
+    documentCursor.row = 0
+    documentCursor.column = 0
+  }
+
+  moveToBottomOfDocument (documentCursor, documentContent) {
+    documentCursor.row = documentContent.length - 1
+    documentCursor.column = 0
+  }
+
+  moveUp (documentCursor, documentContent) {
+    documentCursor.row -= 1
+    if (documentCursor.row < 0) {
+      documentCursor.row = 0
+    }
+    if (documentCursor.column > documentContent[documentCursor.row].length - 1) {
+      this.moveToEndOfLine(documentCursor, documentContent)
+    }
+  }
+
+  moveDown (documentCursor, documentContent) {
+    documentCursor.row += 1
+    if (documentCursor.row > documentContent.length - 1) {
+      documentCursor.row = documentContent.length - 1
+    }
+    if (documentCursor.column > documentContent[documentCursor.row].length - 1) {
+      this.moveToEndOfLine(documentCursor, documentContent)
+    }
+  }
+
+  moveLeft (documentCursor, documentContent) {
+    documentCursor.column -= 1
+    if (documentCursor.column < 0) {
+      if (documentCursor.row > 0) {
+        this.moveUp(documentCursor, documentContent)
+        this.moveToEndOfLine(documentCursor, documentContent)
+      } else {
+        this.moveToStartOfLine(documentCursor, documentContent)
+      }
+    }
+  }
+
+  moveRight (documentCursor, documentContent) {
+    documentCursor.column += 1
+    if (documentCursor.column > documentContent[documentCursor.row].length) {
+      if (documentCursor.row < documentContent.length - 1) {
+        this.moveDown(documentCursor, documentContent)
+        this.moveToStartOfLine(documentCursor, documentContent)
+      } else {
+        this.moveToEndOfLine(documentCursor, documentContent)
+      }
+    }
+  }
+
+  insertCarriageReturn (documentCursor, documentContent) {
+    if (documentCursor.row === documentContent.length - 1) {
+      documentContent.push('FOP')
+      // this.moveDown(documentCursor, documentContent)
+      documentCursor.column = 0
+    } else {
+      documentContent.splice(documentCursor.row + 1, 0, '')
+    }
+  }
+
+  insertBackspace (documentCursor, documentContent) {
+    const changeRow = changes => { documentContent[documentCursor.row] = changes }
+
+    if (documentCursor.column > 0) {
+      documentCursor.column -= 1
+
+      const rowContent = documentContent[documentCursor.row]
+      const pre = rowContent.slice(0, documentCursor.column)
+      const post = rowContent.slice(documentCursor.column + 1)
+      changeRow(`${pre}${post}`)
+    }
+  }
+
+  insertCharacter (character, documentCursor, documentContent) {
+    const rowContent = documentContent[documentCursor.row]
+
+    const changeRow = changes => { documentContent[documentCursor.row] = changes }
+
+    if (documentCursor.column === 0) {
+      changeRow(`${character}${rowContent}`)
+    } else if (documentCursor.column === rowContent.length - 1) {
+      changeRow(`${rowContent}${character}`)
+    } else {
+      const pre = rowContent.slice(0, documentCursor.column)
+      const post = rowContent.slice(documentCursor.column)
+      changeRow(`${pre}${character}${post}`)
+    }
+  }
+
+  onKeyPress (event) {
+    console.log('onKeyPress')
+    console.log({...event})
+
     const {
-      altKey,
+      // altKey,
+      // ctrlKey,
+      // key,
+      // locale,
+      // location,
+      // metaKey,
+      // repeat,
+      // which,
       charCode,
-      ctrlKey,
-      key,
       keyCode,
-      locale,
-      location,
-      metaKey,
-      repeat,
-      shiftKey,
-      which
+      shiftKey
     } = event
+
+    let updateDocument = false
 
     const documentContent = this.state.documentContent.slice()
     const documentCursor = {...this.state.documentCursor}
 
-    const cursorController = {
-      moveToStartOfLine () {
-        documentCursor.column = 0
-      },
-      moveToEndOfLine () {
-        if (documentContent[documentCursor.row].length) {
-          documentCursor.column = documentContent[documentCursor.row].length
-        } else {
-          documentCursor.column = 0
-        }
-      },
-      moveToTopOfDocument () {
-        documentCursor.row = 0
-      },
-      moveToBottomOfDocument () {
-        documentCursor.row = documentContent.length - 1
-      },
-      moveUp () {
-        documentCursor.row -= 1
-        if (documentCursor.row < 0) {
-          documentCursor.row = 0
-        }
-        if (documentCursor.column > documentContent[documentCursor.row].length - 1) {
-          cursorController.moveToEndOfLine()
-        }
-      },
-      moveDown () {
-        documentCursor.row += 1
-        if (documentCursor.row > documentContent.length - 1) {
-          documentCursor.row = documentContent.length - 1
-        }
-        if (documentCursor.column > documentContent[documentCursor.row].length - 1) {
-          cursorController.moveToEndOfLine()
-        }
-      },
-      moveLeft () {
-        documentCursor.column -= 1
-        if (documentCursor.column < 0) {
-          if (documentCursor.row > 0) {
-            cursorController.moveUp()
-            cursorController.moveToEndOfLine()
-          } else {
-            cursorController.moveToStartOfLine()
-          }
-        }
-      },
-      moveRight () {
-        documentCursor.column += 1
-        if (documentCursor.column > documentContent[documentCursor.row].length) {
-          if (documentCursor.row < documentContent.length - 1) {
-            cursorController.moveDown()
-            cursorController.moveToStartOfLine()
-          } else {
-            cursorController.moveToEndOfLine()
-          }
-        }
-      }
+    if (charCode === KEY.ENTER) {
+      updateDocument = true
+      this.insertCarriageReturn(documentCursor, documentContent)
+    } else if (charCode && !keyCode) {
+      updateDocument = true
+      const character = String.fromCharCode(charCode)
+      this.insertCharacter(shiftKey
+        ? character.toUpperCase()
+        : character,
+        documentCursor, documentContent)
+      documentCursor.column += 1
     }
 
-    const KEY = {
-      END: 35,
-      HOME: 36,
-      LEFT: 37,
-      RIGHT: 39,
-      UP: 38,
-      DOWN: 40
+    if (updateDocument) {
+      this.setState({ documentContent, documentCursor })
     }
+  }
 
-    const CURSOR_KEYS = [
-      KEY.UP,
-      KEY.DOWN,
-      KEY.LEFT,
-      KEY.RIGHT
-    ]
-
-    const isKey = (keyMatch) => keyCode === keyMatch
+  onKeyDown (event) {
+    console.log('onKeyDown')
+    console.log({...event})
+    const documentCursor = {...this.state.documentCursor}
+    const documentContent = this.state.documentContent.slice()
 
     let updateCursor = false
+    let updateDocument = false
+
+    const moveToStartOfLine = () => this.moveToStartOfLine(documentCursor, documentContent)
+    const moveToEndOfLine = () => this.moveToEndOfLine(documentCursor, documentContent)
+    const moveToTopOfDocument = () => this.moveToTopOfDocument(documentCursor, documentContent)
+    const moveToBottomOfDocument = () => this.moveToBottomOfDocument(documentCursor, documentContent)
+    const moveUp = () => this.moveUp(documentCursor, documentContent)
+    const moveDown = () => this.moveDown(documentCursor, documentContent)
+    const moveLeft = () => this.moveLeft(documentCursor, documentContent)
+    const moveRight = () => this.moveRight(documentCursor, documentContent)
+
+    const {
+      // altKey,
+      // charCode,
+      // ctrlKey,
+      // key,
+      // locale,
+      // location,
+      // repeat,
+      // shiftKey,
+      // which,
+      keyCode,
+      metaKey
+    } = event
+
+    const isKey = (keyMatch) => keyCode === keyMatch
 
     if (CURSOR_KEYS.indexOf(keyCode) !== -1) {
       updateCursor = true
     }
 
-    if (isKey(KEY.END)) {
+    if (isKey(KEY.BACKSPACE)) {
       event.preventDefault()
-      cursorController.moveToEndOfLine()
+      this.insertBackspace(documentCursor, documentContent)
+      updateCursor = true
+      updateDocument = true
+    } else if (isKey(KEY.END)) {
+      event.preventDefault()
+      moveToEndOfLine()
       updateCursor = true
     } else if (isKey(KEY.HOME)) {
       event.preventDefault()
-      cursorController.moveToStartOfLine()
+      moveToStartOfLine()
       updateCursor = true
     } else if (isKey(KEY.LEFT)) {
       if (metaKey) {
-        cursorController.moveToStartOfLine()
+        moveToStartOfLine()
       } else {
-        cursorController.moveLeft()
+        moveLeft()
       }
     } else if (isKey(KEY.RIGHT)) {
       if (metaKey) {
-        cursorController.moveToEndOfLine()
+        moveToEndOfLine()
       } else {
-        cursorController.moveRight()
+        moveRight()
       }
     } else if (isKey(KEY.DOWN)) {
       if (metaKey) {
-        cursorController.moveToBottomOfDocument()
+        moveToBottomOfDocument()
       } else {
-        cursorController.moveDown()
+        moveDown()
       }
     } else if (isKey(KEY.UP)) {
       if (metaKey) {
-        cursorController.moveToTopOfDocument()
+        moveToTopOfDocument()
       } else {
-        cursorController.moveUp()
+        moveUp()
       }
     }
 
     const nextState = {}
+
+    if (updateDocument) {
+      nextState.documentContent = documentContent
+    }
 
     if (updateCursor) {
       nextState.documentCursor = documentCursor
     }
 
     this.setState(nextState)
-
-
-    const props = {
-      altKey,
-      charCode,
-      ctrlKey,
-      key,
-      keyCode,
-      locale,
-      location,
-      metaKey,
-      repeat,
-      shiftKey,
-      which
-    }
-
-    const report = Object.keys(props).map(name => {
-      const value = props[name]
-      return `${name}: ${value},`
-    })
-
-    report[report.length - 1] = report[report.length - 1].substr(0, report[report.length - 1].length - 2)
-
-    report.splice(0, 0, '{')
-    report.push('}')
-
-    console.log(JSON.parse(JSON.stringify(report.join(''))))
   }
 
   render () {
@@ -216,6 +314,7 @@ export default class Notepad extends Component {
         className='top-level-window'
         tabIndex={0}
         onKeyDown={this.onKeyDown}
+        onKeyPress={this.onKeyPress}
         ref={(element) => { this.topLevel = element }}
         >
         <div className='notepad__header'>Notepad v1.0 - Untitled.txt</div>
